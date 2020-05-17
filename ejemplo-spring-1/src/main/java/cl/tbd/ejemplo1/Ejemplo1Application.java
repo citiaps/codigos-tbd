a7d592ab59c5bf109d545c7ebf10bf47ca5d47a8
dfao <diego.avila@usach.cl>
2020-05-16 20:58:51 -0400

    sin SecurityAutoConfiguration

--- ejemplo-spring-1/src/main/java/cl/tbd/ejemplo1/Ejemplo1Application.java
+++ ejemplo-spring-1/src/main/java/cl/tbd/ejemplo1/Ejemplo1Application.java
@@ -2,11 +2,10 @@ package cl.tbd.ejemplo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @RestController
 public class Ejemplo1Application {
 
